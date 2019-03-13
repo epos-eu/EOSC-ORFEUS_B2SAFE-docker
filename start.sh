4a2747a48120578a875b198b9657ec4cdb4bd22f
Massimo Fares <massimo.fares@ingv.it>
2019-03-13 12:35:10 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ docker start eudat_b2safe
 
 docker exec -it eudat_b2safe bash -c 'service postgresql start'
 
-until docker exec -it --user postgres eudat_b2safe psql -c "select 1" > /dev/null 2>&1; do sleep 2; done
+# until docker exec -it --user postgres eudat_b2safe psql -c "select 1" > /dev/null 2>&1; do sleep 2; done
 
 docker exec -it --user irods eudat_b2safe /var/lib/irods/irodsctl start
